cac1d97810cae48476b10967a87a6e314bf3c04a
ssosnik <ssosnik@gmail.com>
2023-08-19 12:57:33 +0200

    Add entity classes App and Review

---
+++ src/main/java/com/ssosnik/apprating/domain/DataInitializer.java
@@ -0,0 +1,4 @@
+package com.ssosnik.apprating.domain;
+
+public class DataInitializer {
+}
---
+++ src/main/java/com/ssosnik/apprating/domain/Review.java
@@ -0,0 +1,23 @@
+package com.ssosnik.apprating.domain;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Review {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int rating;
+    private int reviewerAge;
+    private String reviewerCountry;
+    private LocalDate date;
+
+    @ManyToOne
+    private App app;
+}
